37a5797fc70475543eea5f5b1d53db59b05b1390
Kaken Bok <bok@kakenbok.de>
2021-02-09 19:08:15 +0100

    add afeefa/package-manager

---
+++ .afeefa/package/release/release.php
@@ -0,0 +1,14 @@
+<?php
+
+use Afeefa\Component\Package\Package\Package;
+use Afeefa\Component\Package\ReleaseManager;
+use Webmozart\PathUtil\Path;
+
+return (new ReleaseManager())
+    ->packages([
+        Package::composer()
+            ->path(Path::join(getcwd(), 'api-resources-server')),
+
+        Package::npm()
+            ->path(Path::join(getcwd(), 'api-resources-client'))
+    ]);
