6bec4e34cdc4d0538b246bde00060969068461bd
Kaken Bok <bok@kakenbok.de>
2021-02-22 16:42:10 +0100

    dist -> lib

--- api-resources-client/dist/es5/Client.js
+++
@@ -1,9 +0,0 @@
-var Client = /** @class */ (function () {
-    function Client() {
-    }
-    Client.prototype.get = function () {
-        return 'GET';
-    };
-    return Client;
-}());
-export { Client };
--- api-resources-client/src/Client.ts
+++ api-resources-client/src/Client.ts
@@ -1,5 +1,23 @@
 export class Client {
   get (): string {
-    return 'GET'
+
+    const digits = new Map([
+      [0, "zero"],
+      [1, "one"],
+      [2, "two"],
+      [3, "three"],
+      [4, "four"],
+      [5, "five"],
+      [6, "six"],
+      [7, "seven"],
+      [8, "eight"],
+      [9, "nine"]
+    ]);
+
+    for (const [digit, name] of digits) {
+      console.log(`${digit} -> ${name}`);
+    }
+
+    return 'GET GET'
   }
 }
