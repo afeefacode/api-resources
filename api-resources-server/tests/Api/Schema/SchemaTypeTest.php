ec89ef765a079d0bc397f07a13b3020715cad503
Kaken Bok <bok@kakenbok.de>
2021-10-19 13:55:56 +0200

    update tests + test api builder

--- api-resources-server/tests/Api/Schema/SchemaTypeTest.php
+++ api-resources-server/tests/Api/Schema/SchemaTypeTest.php
@@ -2,19 +2,22 @@
 
 namespace Afeefa\ApiResources\Tests\Api;
 
+use Afeefa\ApiResources\Action\Action;
 use Afeefa\ApiResources\Field\FieldBag;
 use Afeefa\ApiResources\Field\Fields\HasOneRelation;
 use Afeefa\ApiResources\Field\Fields\VarcharAttribute;
-use Afeefa\ApiResources\Tests\Fixtures\SingleTypeApi\SingleTypeApi;
+use Afeefa\ApiResources\Test\ApiBuilder;
+use Afeefa\ApiResources\Test\TestApi;
+use Afeefa\ApiResources\Test\TestResource;
 use Afeefa\ApiResources\Type\Type;
 use PHPUnit\Framework\TestCase;
 
-class SchemaTypeTest extends TestCase
+class TypeTest extends TestCase
 {
     public function test_simple()
     {
         $type = new class() extends Type {
-            public static string $type = 'Test.SchemaType';
+            public static string $type = 'Test.Type';
 
             protected function fields(FieldBag $fields): void
             {
@@ -25,15 +28,23 @@ class SchemaTypeTest extends TestCase
             }
         };
 
-        $api = SingleTypeApi::create(get_class($type));
+        $api = (new ApiBuilder())
+            ->api('Test.Api', function (TestApi $api) use ($type) {
+                $api->resource('Test.Resource', function (TestResource $resource) use ($type) {
+                    $resource->action('test_action', function (Action $action) use ($type) {
+                        $action->response(get_class($type));
+                    });
+                });
+            })
+            ->get();
 
-        debug_dump($api->toSchemaJson());
+        // debug_dump($api->toSchemaJson());
 
         $schema = $api->toSchemaJson();
 
-        $this->assertIsArray($schema['types']['Test.SchemaType']);
+        $this->assertIsArray($schema['types']['Test.Type']);
 
-        $typeSchema = $schema['types']['Test.SchemaType'];
+        $typeSchema = $schema['types']['Test.Type'];
         $fieldsSchema = $typeSchema['fields'];
 
         $this->assertEquals(['title', 'name', 'related_type'], array_keys($fieldsSchema));
@@ -41,7 +52,7 @@ class SchemaTypeTest extends TestCase
         $this->assertEquals(VarcharAttribute::$type, $fieldsSchema['name']['type']);
         $this->assertEquals(VarcharAttribute::$type, $fieldsSchema['title']['type']);
         $this->assertEquals(HasOneRelation::$type, $fieldsSchema['related_type']['type']);
-        $this->assertEquals('Test.SchemaType', $fieldsSchema['related_type']['related_type']);
+        $this->assertEquals('Test.Type', $fieldsSchema['related_type']['related_type']);
 
         $this->assertEquals([], $schema['validators']);
         $this->assertArrayNotHasKey('update_fields', $typeSchema);
