d9f4d1bf4f250ccb124980f9fe27d4a4f7e4a744
Kaken Bok <bok@kakenbok.de>
2022-07-17 15:54:23 +0200

    allow hiding attributes from being cloned at all

--- api-resources-client/src/Model.ts
+++ api-resources-client/src/Model.ts
@@ -108,7 +108,11 @@ export class Model {
     }
 
     for (const name of Object.keys(typeFields)) {
-      if (!this[name]) { // value not set, just copy
+      if (fields && fields[name] === false) { // ignore deactivated fields
+        continue
+      }
+
+      if (!this[name]) { // value not set (undefined or null or false or '' or 0), just copy
         // console.log('novalue-copy', this, name)
         model[name] = this[name]
       } else {
