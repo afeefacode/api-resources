bad392f685566a5e990cf514df730419055b058f
Kaken Bok <bok@kakenbok.de>
2022-11-17 11:01:22 +0100

    update example

--- example/frontend/src/components/SidebarMenu.vue
+++ example/frontend/src/components/SidebarMenu.vue
@@ -1,11 +1,13 @@
 <template>
-  <app-sidebar-menu :config="config" />
+  <app-sidebar-menu :items="items" />
 </template>
 
 <script>
 import { Component, Vue } from 'vue-property-decorator'
 import AppSidebarMenu from '@a-admin/components/SidebarMenu'
-import { MenuConfig } from '@/config/MenuConfig'
+import ModelCount from '@a-admin/components/model/ModelCount'
+import { apiResources } from '@afeefa/api-resources-client'
+import { mdiAccount, mdiFileDocumentOutline } from '@mdi/js'
 
 @Component({
   components: {
@@ -13,10 +15,41 @@ import { MenuConfig } from '@/config/MenuConfig'
   }
 })
 export default class SidebarMenu extends Vue {
-  config = null
+  get items () {
+    return [
+      {
+        title: 'Artikel',
+        to: {
+          name: 'articles.list'
+        },
+        icon: mdiFileDocumentOutline,
+        iconColor: 'yellow darken-3',
+        badge: this.getBadge('articles')
+      },
 
-  created () {
-    this.config = new MenuConfig()
+      {
+        title: 'Autor:innen',
+        to: {
+          name: 'authors.list'
+        },
+        icon: mdiAccount,
+        iconColor: 'yellow darken-3',
+        badge: this.getBadge('authors')
+      }
+    ]
+  }
+
+  getBadge (field) {
+    return {
+      component: ModelCount,
+      props: {
+        action: apiResources.getAction({
+          resourceType: 'Example.AppResource',
+          actionName: 'get_counts'
+        }),
+        field: 'count_' + field
+      }
+    }
   }
 }
 </script>
