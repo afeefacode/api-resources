9e42ae3583893a58c96cc11dd5c4ed3baef9c2c3
Kaken Bok <bok@kakenbok.de>
2022-12-27 10:38:24 +0100

    remove useless selectvalidator

--- api-resources-client/src/validator/validators/index.ts
+++ api-resources-client/src/validator/validators/index.ts
@@ -2,13 +2,11 @@ import { DateValidator } from './DateValidator'
 import { IntValidator } from './IntValidator'
 import { LinkOneValidator } from './LinkOneValidator'
 import { NumberValidator } from './NumberValidator'
-import { SelectValidator } from './SelectValidator'
 import { StringValidator } from './StringValidator'
 
 export const validators = {
   'Afeefa.StringValidator': new StringValidator(),
   'Afeefa.LinkOneValidator': new LinkOneValidator(),
-  'Afeefa.SelectValidator': new SelectValidator(),
   'Afeefa.NumberValidator': new NumberValidator(),
   'Afeefa.IntValidator': new IntValidator(),
   'Afeefa.DateValidator': new DateValidator()
